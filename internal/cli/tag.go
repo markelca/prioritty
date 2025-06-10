0a33a833902613f2103d0f76a647960bc9e3642b
Markel Cuesta <cuestaarribas.markel@proton.me>
2025-06-10 19:15:17 +0200

    Modifying multiple tags with one command

--- internal/cli/tag.go
+++ internal/cli/tag.go
@@ -1,7 +1,6 @@
 package cli
 
 import (
-	"fmt"
 	"log"
 	"strconv"
 
@@ -15,46 +14,63 @@ func init() {
 }
 
 var tagCmd = &cobra.Command{
-	Use:     "tag {id} {tag}",
+	Use:     "tag {tag} {id...}",
 	Aliases: []string{},
-	Args:    cobra.ExactArgs(2),
-	Short:   "Sets the tag for a task",
+	Args:    cobra.MinimumNArgs(2),
+	Short:   "Sets the tag for one or more tasks",
+	Long:    `Sets the tag for one or more tasks by providing the tag name and their IDs`,
 	Run: func(cmd *cobra.Command, args []string) {
 		m := tui.InitialModel(false)
-		i, err := strconv.Atoi(args[0])
-		if err != nil {
-			fmt.Println("Error converting string index to int")
-		}
+		tag := args[0]
 
-		item := m.GetItemAt(i - 1)
-		if item == nil {
-			fmt.Printf("Error - No item found with index %d", i)
-		}
-		err = m.Service.SetTag(item, args[1])
-		if err != nil {
-			log.Printf("Error setting the task: %v", err)
+		for _, arg := range args[1:] {
+			index, err := strconv.Atoi(arg)
+			if err != nil {
+				log.Printf("Error: Invalid task ID '%s'. Please provide a valid number.\n", arg)
+				continue
+			}
+
+			item := m.GetItemAt(index - 1)
+			if item == nil {
+				log.Printf("Task at index %d does not exist", index)
+				continue
+			}
+
+			err = m.Service.SetTag(item, tag)
+			if err != nil {
+				log.Printf("Error setting tag for task %d: %v\n", index, err)
+				continue
+			}
 		}
 	},
 }
 
 var tagUnsetCmd = &cobra.Command{
-	Use:   "unset {id}",
-	Args:  cobra.ExactArgs(1),
-	Short: "Unsets the tag for a task or note",
+	Use:   "unset {id...}",
+	Args:  cobra.MinimumNArgs(1),
+	Short: "Unsets the tag for one or more tasks or notes",
+	Long:  `Unsets the tag for one or more tasks or notes by providing their IDs`,
 	Run: func(cmd *cobra.Command, args []string) {
 		m := tui.InitialModel(false)
-		i, err := strconv.Atoi(args[0])
-		if err != nil {
-			fmt.Println("Error converting string index to int")
-		}
 
-		item := m.GetItemAt(i - 1)
-		if item == nil {
-			fmt.Printf("Error - No item found with index %d", i)
-		}
-		err = m.Service.UnsetTag(item)
-		if err != nil {
-			log.Printf("Error unsetting the tag: %v", err)
+		for _, arg := range args {
+			index, err := strconv.Atoi(arg)
+			if err != nil {
+				log.Printf("Error: Invalid task ID '%s'. Please provide a valid number.\n", arg)
+				continue
+			}
+
+			item := m.GetItemAt(index - 1)
+			if item == nil {
+				log.Printf("Task at index %d does not exist", index)
+				continue
+			}
+
+			err = m.Service.UnsetTag(item)
+			if err != nil {
+				log.Printf("Error unsetting tag for task %d: %v\n", index, err)
+				continue
+			}
 		}
 	},
 }
